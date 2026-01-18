ecdf33750d888607d61ea909fc525df9189bd8bf
weilok2021 <weilok629@gmail.com>
2026-01-18 08:17:28 +0000

    Sun, Jan 18, 2026, 4:17 PM +08:00

--- week1/mario-less/mario.c
+++ week1/mario-less/mario.c
@@ -2,7 +2,16 @@
 #include <stdio.h>
 
 int main(void) {
-    for (int i = 0; i < 3; i++) {
-        
+    for (int i = 0; i < 5; i++) {
+        int num_spaces = 5 - (i+1);
+        while (num_spaces != 0) {
+            printf(" ");
+            num_spaces--;
+        }
+
+        for (int j = 0; j < i + 1; j++) {
+            printf("#");
+        }
+        printf("\n");
     }
 }
