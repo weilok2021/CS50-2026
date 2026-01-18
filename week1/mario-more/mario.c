37563664f5eccacf3aaf27365ba0cb41b10cad3a
weilok2021 <weilok629@gmail.com>
2026-01-18 08:43:58 +0000

    Sun, Jan 18, 2026, 4:43 PM +08:00

--- week1/mario-more/mario.c
+++ week1/mario-more/mario.c
@@ -1,3 +1,29 @@
+#include <cs50.h>
 #include <stdio.h>
 
+void print_pyramid(int height);
 
+int main(void) {
+    int height;
+    do {
+        height = get_int("Height: ");
+    }
+    while(height <= 0);
+
+    print_pyramid(height);
+}
+
+void print_pyramid(int height) {
+    for (int i = 0; i < height; i++) {
+        int num_spaces = height - (i + 1);
+        while (num_spaces != 0) {
+            printf(" ");
+            num_spaces--;
+        }
+
+        for (int j = 0; j < i + 1; j++) {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
