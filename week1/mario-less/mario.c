db1dbf84791e2a706f69d03ac1eef45e15fec6bb
weilok2021 <weilok629@gmail.com>
2026-01-18 08:29:32 +0000

    Sun, Jan 18, 2026, 4:29 PM +08:00

--- week1/mario-less/mario.c
+++ week1/mario-less/mario.c
@@ -1,13 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void print_pyramid(int height);
+
 int main(void) {
     int height;
     do {
         height = get_int("Height: ");
     }
     while(height <= 0);
-    
+    print_pyramid(height);
 }
 
 void print_pyramid(int height) {
